954d278e4e8850ab989896c05019ae107941901f
Grino777 <bars_B_P@mail.ru>
2022-08-30 20:31:00 +0300

    Add classes

--- parser.py
+++ parser.py
@@ -1,18 +1,29 @@
-from pydantic import BaseModel
 import json
+from classes import *
 
-# def incoming_text(data=None):
-
-
+def get_incoming_text():
+    with open('data.txt', 'r', encoding='utf-8') as file:
+        data = file.read().replace('\n', ' ')
+        # data = json.loads(data)
+    return data
 
 def main():
+    # print(type(get_incoming_text()))
 
-    with open('data.txt', 'r', encoding='utf-8') as file:
-        data = file.read().replace('\n', '')
-        data = json.loads(data)
+    try:
+        data = Data.parse_raw(get_incoming_text())
+        # for k, v in data.__fields__.items():
+        #     print(k, v)
+        print(data.json(by_alias=True))
 
-    with open('data.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
+    except ValidationError as e:
+        print(e)
+
 
 if __name__ == '__main__':
     main()
+
+# ,
+#     "schedule" : {
+#         "id": "Never",
+#     }
